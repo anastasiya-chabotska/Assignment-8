a350d54e6c3cbcf8007f7f73f0832fa6942d5162
medmoe <med.seffah@gmail.com>
2021-01-17 11:34:28 -0500

    added some styling

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,16 +4,23 @@ class NavBar extends Component {
     
     render(){
         return <div>
-            <button onClick={this.props.addrow}>add row</button>
-            <button onClick={this.props.addcol}>add col</button>
-            <button onClick={this.props.removeRow}>remove row</button>
-            <button onClick={this.props.removeCol}>remove col</button>
-            <button onClick={this.props.clearAll}>clear all</button>
-            <select onChange={this.props.setColor}>
-                <option value="#FF0000">Red</option>
-                <option value="#00FF00">Green</option>
-                <option value="#0000FF">Blue</option>
-            </select>
+            <div className="buttons">
+                <button onClick={this.props.addrow} className="btn-success btn-lg">add row</button>
+                <button onClick={this.props.addcol} className="btn-success btn-lg">add col</button>
+                <button onClick={this.props.removeRow} className="btn-success btn-lg">remove row</button>
+                <button onClick={this.props.removeCol} className="btn-success btn-lg">remove col</button>
+                <button onClick={this.props.clearAll} className="btn-success btn-lg">clear all</button>
+                <button onClick={this.props.fillAll} className="btn-success btn-lg">fill all</button>
+                <button onClick={this.props.fillUncolored} className="btn-success btn-lg">fillUncolored</button>
+            </div>
+            <div className="select-container"> 
+                <select onChange={this.props.setColor}>
+                    <option value="#FF0000">Red</option>
+                    <option value="#00FF00">Green</option>
+                    <option value="#0000FF">Blue</option>
+                </select>
+            </div>
+            
             
         </div>
     }
